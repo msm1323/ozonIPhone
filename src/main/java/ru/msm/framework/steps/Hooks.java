63eb0cd57952748232d4857675da0f9a741956bb
MagomedovaSvetlana13 <svetlana.magomedova.1998@gmail.com>
2021-10-24 15:39:58 +0300

    fixed hook createProductsInfo()

--- src/main/java/ru/msm/framework/steps/Hooks.java
+++ src/main/java/ru/msm/framework/steps/Hooks.java
@@ -4,14 +4,8 @@ import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
-import io.qameta.allure.model.StepResult;
-import io.qameta.allure.util.ResultsUtils;
 import ru.msm.framework.managers.DataManager;
 import ru.msm.framework.managers.InitManager;
-
-import java.util.UUID;
-
-import static io.qameta.allure.model.Status.BROKEN;
 
 public class Hooks {
 
@@ -23,40 +17,18 @@ public class Hooks {
     }
 
     @Before
-    public void before(){
+    public void before() {
         InitManager.initFramework();
     }
 
     @After(order = 500)
-    public void createProductsInfo(){
-        step("Список всех товаров и товар с наибольшей ценой.", () -> {
-            Allure.getLifecycle().addAttachment("ProductsInfo", "text/plain", "txt", getProductsInfo());
-        });
+    public void createProductsInfo() {
+        Allure.getLifecycle().addAttachment("ProductsInfo", "text/plain", "txt", getProductsInfo());
     }
 
     @After(order = 100)
-    public void after(){
+    public void after() {
         InitManager.quitFramework();
-    }
-
-    public static void step(String name, Runnable runnable) {
-        String uuid = UUID.randomUUID().toString();
-        StepResult result = new StepResult()
-                .setName(name)
-                .setStart(System.currentTimeMillis());  //!
-        Allure.getLifecycle().startStep(uuid, result);
-        try {
-            runnable.run();
-            Allure.getLifecycle().updateStep(uuid, s -> s.setStatus(
-                    io.qameta.allure.model.Status.PASSED));
-        } catch (Throwable e) {
-            Allure.getLifecycle().updateStep(uuid, s -> s
-                    .setStatus(ResultsUtils.getStatus(e).orElse(BROKEN))
-                    .setStatusDetails(ResultsUtils.getStatusDetails(e).orElse(null)));
-            throw e;
-        } finally {
-            Allure.getLifecycle().stopStep(uuid);
-        }
     }
 
 }
